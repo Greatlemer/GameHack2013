288bbf5a1de3ceca6c0de75a61c385711ac1df56
David Schneider <dave.schneider@gmail.com>
2013-11-16 20:33:37 +0000

    Added controllable paint bomb launcher and water hose

---
+++ Assets/Fire.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var projectile : GameObject;
+var key : String;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetButtonDown(key))
+	{
+		GameObject.Instantiate(projectile, transform.position, transform.rotation * projectile.transform.rotation);
+	}
+}
