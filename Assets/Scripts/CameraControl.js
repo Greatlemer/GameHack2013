e2a09169deb5fd57b4be7df6dbce7c8bdd091fd8
Adrian Rogers <greatlemer@gmail.com>
2013-11-28 00:02:33 +0000

    Camera follows the characters

---
+++ Assets/Scripts/CameraControl.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+private var activeCharacter : Transform;
+public var maxX = 9.2;
+public var minX = -9.2;
+
+function Awake () {
+	GameControl.RegisterCamera(this);
+}
+
+function ChangeFocus(newCharacter : ControlCharacter) {
+	this.activeCharacter = newCharacter.transform;
+	this.UpdateX();
+}
+
+function UpdateX() {
+	var newX = this.activeCharacter.position.x;
+	newX = Mathf.Clamp(newX, minX, maxX);
+	this.transform.position.x = newX;
+}
+
+function Update() {
+	this.UpdateX();
+}
--- Assets/Scripts/GameControl.js
+++ Assets/Scripts/GameControl.js
@@ -4,6 +4,7 @@ var current_team = 1;
 private static var characters : ControlCharacter[];
 private static var activeCharacter = 0;
 private static var characterCount = 0;
+private static var activeCamera : CameraControl;
 
 function Start () {
 
@@ -43,13 +44,24 @@ static function RegisterCharacter(newCharacter : ControlCharacter) {
 	if(!characters) {
 		characters = new ControlCharacter[16];
 		newCharacter.Activate();
+		if (activeCamera) {
+			activeCamera.ChangeFocus(newCharacter);
+		}
 	}
 	characters.SetValue(newCharacter, characterCount);
 	characterCount++;
+}
+
+static function RegisterCamera(newCamera : CameraControl) {
+	activeCamera = newCamera;
+	if (characters) {
+		activeCamera.ChangeFocus(characters[activeCharacter]);
+	}
 }
 
 function ActivateCharacter(newCharacter : int) {
 	characters[activeCharacter].Deactivate();
 	characters[newCharacter].Activate();
 	activeCharacter = newCharacter;
+	activeCamera.ChangeFocus(characters[activeCharacter]);
 }
